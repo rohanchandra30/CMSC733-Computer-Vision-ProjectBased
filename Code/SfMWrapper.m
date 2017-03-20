99d8db2d5461981cc29fa83a7c0cc7fab4859362
Swati Singhal <Swati@wireless-10-104-229-28.umd.edu>
2017-03-20 18:37:19 -0400

    RANSAC and fundamental matrix

--- Code/SfMWrapper.m
+++ Code/SfMWrapper.m
@@ -9,39 +9,24 @@ Nimages = 6;
 K = [568.996140852 0 643.21055941; 0 568.988362396 477.982801038; 0 0 1];
 
 %% Load the images and extract matching%%
-
-
-for i = 1:Nimages
-    %Images(i) = imread(sprintf('%s/%d.jpg',Imagepath, i));
-    filename = fullfile(Imagepath,sprintf('matching%d.txt',i));
-    fileID = fopen(filename);
-    nfeatures = fscanf(fileID,'nFeatures: %d');
-    
-    if i ~= Nimages
-      nFeatures = textread(sprintf('%s/matching%d.txt', Imagepath, i), 'nnFeatures: %d');
-      field1 = 'nFeatures';
-      value1 = nFeatures;
-      for j = 1:nFeatures
-          [nMatches r g b x y] = textread(sprintf('%s/matching%d.txt', Imagepath, i), '%d %d %d %d %f %f');
-          
-      end   
-    end  
-   fclose(fileID);  
-end    
+for i = 1:Nimages-1
+ load(sprintf('matching%d',i));
+end
+matches = cell(Nimages-1,1);
+matches{1} = parseMatching(Nimages, matching1, 1);
+matches{2} = parseMatching(Nimages, matching2, 2);
+matches{3} = parseMatching(Nimages, matching3, 3);
+matches{4} = parseMatching(Nimages, matching4, 4);
+matches{5} = parseMatching(Nimages, matching5, 5);
 
 % -------------------------------------------------------Rohan's Commit-----------------------------------------------------------------------
-% Testing Part 5
 
-% Dummy F...
-F = ones(3,3);
+for i=1:Nimages-1
+   for j = i:Nimages
+       [y1,y2,idx] = GetInliersRANSAC(matches{i,1}{i,1},matches{i,1}{j,1});
+   end    
+end    
 
-% K is given...
-K = [568.996140852 0 643.21055941;
-     0 568.988362396 477.982801038;
-     0 0 1];
-
- 
- % Part 5
-clc
+% Part 5
 E = EssentialMatrixFromFundamentalMatrix(F,K);
 [Cset,Rset] = ExtractCameraPose(E); 

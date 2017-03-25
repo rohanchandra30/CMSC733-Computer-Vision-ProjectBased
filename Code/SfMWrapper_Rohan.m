a65b209739d59e620339215166b69f8bf8730712
Swati Singhal <Swati@Swatis-MacBook-Pro.local>
2017-03-24 22:38:09 -0400

    New changes for getting image 3 points

--- Code/SfMWrapper_Rohan.m
+++ Code/SfMWrapper_Rohan.m
@@ -80,47 +80,45 @@ Display3D({zeros(3,1), C}, {eye(3), R}, X_opt);
 
 %% Part 7 and 8
 
-Cset = cell(Nimages-1, 1);
-Rset = cell(Nimages-1, 1);
+Cset = cell(Nimages, 1);
+Rset = cell(Nimages, 1);
+Xset = cell(Nimages, 1);
 Rset{1,1} = eye(3);
 Cset{1,1} = zeros(3,1);
 Rset{2,1} = R;
 Cset{2,1} = C;
+Xset{1} = [];
+Xset{2} = X_opt;
+for i = 3:4
 
-for i = 2:2
-%     x_n = [];
-%     X_n = [];
-%     for j = 1:length(x1)
-%       in = [];
-%       in = find(points{i-1,i+1}(:,1) == x1(j,1)  & points{i-1,i+1}(:,2) == x1(j,2));
-%       if ~isempty(in) 
-%              %fprintf('Point %d is mapping to 2 points\n', j);    
-%              x_n = [x_n; points{i-1,i+1}(in(1),3:4)];
-%              X_n = [X_n; X_opt(j,:)];
-%       end    
-%     end
-    x_n = data.x3;
-    X_n = X_opt;
- 
-    if length(x_n) < 6
+    id = intersect(indx{i-2, i-1}, find(cellfun(@(x) ~isempty(x), fpoints(:,i))));
+    x = cell2mat(fpoints(id,i));
+    I = [];
+    for m = 1:length(id)
+      I = [I; find(indx{i-2, i-1} == id(m))];
+    end
+    X_n = Xset{i-1}(I>0,:);
+    if length(x) < 6
         fprintf('Need atleast 6 correspondenses\n');
         continue;
     end    
-    [Cnew,Rnew] = PnPRANSAC(X_n, x_n, K);
-    [Cnew, Rnew] = NonlinearPnP(X_n, x_n, K, Cnew, Rnew);
+    [Cnew,Rnew] = PnPRANSAC(X_n, x, K);
+    [Cnew, Rnew] = NonlinearPnP(X_n, x, K, Cnew, Rnew);
+
     Cset{i,1} = Cnew;
     Rset{i,1} = Rnew;
     
-%     x1 = Points{i,i+1}(:,1:2);
-%     x2 = Points{i,i+1}(:,3:4);
-%     
-    x1 = data.x2
-    x2 = data.x3;
-    
+    x1 = Points{i-1,i}(:,1:2);
+    x2 = Points{i-1,i}(:,3:4);
+      
     X_new = LinearTriangulation(K, Cset{i-1}, Rset{i-1}, Cset{i}, Rset{i}, x1, x2) ;
-    X_new = NonlinearTriangulation(K, Cset{i-1}, Rset{i-1}, Cset{i}, Rset{i}, x1, x2, X_new);
+    [X_new, id] = NonlinearTriangulation(K, Cset{i-1}, Rset{i-1}, Cset{i}, Rset{i}, x1, x2, X_new);
+    X_new = X_new(id>0, :);
+    x1 = x1(id>0, :);
+    x2 = x2(id>0, :);
+    indx{i-1,i} = indx{i-1,i}(id>0,:);
 
-    X = [X; X_new];
+    Xset{i} = X_new;
     
     %Building traj
     %traj = cell(N, 1);
@@ -129,7 +127,7 @@ for i = 2:2
     DisplayCorrespondence(I3, x2, t2);
 
    
-    V = BuildVisibilityMatrix(Nimages, X);
+    V = BuildVisibilityMatrix(Nimages, Xset);
 
     %[Cset, Rset, X] = BundleAdjustment(Cset, Rset, X, K, traj, V);
 end
